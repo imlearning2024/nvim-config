3c74fbf317929f3fe98191e9d3cbbbc81edb27df
Ajay_Giri <imlearningcoding2024@gmail.com>
2025-04-03 00:21:23 +0530

    added-smear-animation

--- lua/plugins/auto-save.lua
+++ lua/plugins/auto-save.lua
@@ -2,7 +2,7 @@ return{
   "okuuva/auto-save.nvim",
   config = function()
     require("auto-save").setup({
-      execution_message = "Auto-Saving...",
+--      execution_message = "Auto-Saving...",
       debounce_delay = 100,
       condition = function(buf)
         local fn = vim.fn
